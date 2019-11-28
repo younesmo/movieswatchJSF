3ee200e76aa22fc9fb6204f599f1568a0fb86a90
younes.moumtaz <younes.moumtaz@gmail.com>
2019-11-28 15:23:40 +0100

    bug corrected

--- src/com/movieswatch/managedBeans/MoviesBean.java
+++ src/com/movieswatch/managedBeans/MoviesBean.java
@@ -24,6 +24,8 @@ import com.movieswatch.services.MovieServiceImpl;
 public class MoviesBean implements Serializable{
 	
 	private List<Movie> movies;
+	private List<Movie> longMovies= new ArrayList<Movie>();
+	private List<Movie> shortMovies= new ArrayList<Movie>();
 	transient private MovieService movieService;
 	transient private static Logger logger = Logger.getLogger(MoviesBean.class);
 	private int idMovie;
@@ -37,6 +39,18 @@ public class MoviesBean implements Serializable{
 	@PostConstruct
 	public void init() {
 		movies= movieService.getMovies();
+		sortMovies();
+	}
+	
+	public void sortMovies() {
+		longMovies= new ArrayList<Movie>();
+		shortMovies= new ArrayList<Movie>();
+		for(Movie movie: movies) {
+			if(movie.getMetrage().equals("long"))
+				longMovies.add(movie);
+			else
+				shortMovies.add(movie);
+		}
 	}
 	
 	public void updateList() {
@@ -111,6 +125,7 @@ public class MoviesBean implements Serializable{
 	}
 
 		movies= moviesToSend;
+		sortMovies();
 	}
 	
 	public String goToAddMovie() {
@@ -149,10 +164,21 @@ public class MoviesBean implements Serializable{
 	public void setIdMovie(int idMovie) {
 		this.idMovie = idMovie;
 	}
-	
-	
-	
-	
-	
+
+	public List<Movie> getLongMovies() {
+		return longMovies;
+	}
+
+	public void setLongMovies(List<Movie> longMovies) {
+		this.longMovies = longMovies;
+	}
+
+	public List<Movie> getShortMovies() {
+		return shortMovies;
+	}
+
+	public void setShortMovies(List<Movie> shortMovies) {
+		this.shortMovies = shortMovies;
+	}	
 	
 }
