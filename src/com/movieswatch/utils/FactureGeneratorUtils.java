318aec3ad8eb75d162f1a145fbdc35025dc0d417
Souhaïb Jemaiel <jemaielsouhaib@hotmail.fr>
2020-01-22 15:13:39 +0100

    FactureGenerator

--- src/com/movieswatch/utils/FactureGeneratorUtils.java
+++ src/com/movieswatch/utils/FactureGeneratorUtils.java
@@ -21,6 +21,7 @@ import com.itextpdf.text.List;
 import com.itextpdf.text.ListItem;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.Phrase;
+import com.itextpdf.text.Rectangle;
 import com.itextpdf.text.Section;
 import com.itextpdf.text.pdf.PdfContentByte;
 import com.itextpdf.text.pdf.PdfImage;
@@ -37,33 +38,72 @@ import com.movieswatch.entities.User;
 public class FactureGeneratorUtils {
 
 	public static String imageFile;
+	
 	private static Font titleFont = new Font(Font.FontFamily.TIMES_ROMAN, 18,
             Font.BOLD);
     private static Font redFont = new Font(Font.FontFamily.TIMES_ROMAN, 12,
             Font.NORMAL, BaseColor.RED);
     private static Font subFont = new Font(Font.FontFamily.TIMES_ROMAN, 16,
             Font.BOLD);
-    private static Font smallBold = new Font(Font.FontFamily.TIMES_ROMAN, 12,
-            Font.BOLD);
+    private static Font smallItalic = new Font(Font.FontFamily.TIMES_ROMAN, 12,
+            Font.ITALIC);
     
+    
+    public static PdfPCell ParagraphCell(int alignment , String paragraph, int border , Font font)
+	 {
+		 
+		 Paragraph Paragraph = new Paragraph(paragraph ,font);
+		 PdfPCell Cell = new PdfPCell(Paragraph);
+		 Cell.setBorder(border);
+		 Cell.setHorizontalAlignment(alignment);
+		 return Cell;
+	 }
+  
+ 
 	public static void generateFacture(User user, Order order, ServletContext servletContext) throws MalformedURLException, IOException {
 		try {
 			
 			Document document = new Document();
-			document.open();
-			/* imageFile = servletContext.getResource("/resources/default/images/avatars/movieswatch.jpg").getPath();
-			
+			imageFile = servletContext.getResource("/resources/default/images/avatars/movieswatchlogo.jpg").getPath();
 			PdfWriter.getInstance(document,new FileOutputStream(servletContext.getRealPath("/")+"/bills/" +order.getId()+".pdf"));
 			
-			  Image img = Image.getInstance(imageFile);
-		        PdfPCell cell = new PdfPCell(img, true);
-		        PdfPTable table = new PdfPTable(1);
-		        table.addCell(cell);
-		       */
-			document.add(new Paragraph("Nom: " + user.getLastname() + " Prenom :" + user.getFirstname()));
-				document.add(new Paragraph("Adresse mail: " + user.getEmail()));
-				document.add(new Paragraph("Adresse: " + user.getStreetName() +" "+ user.getPostalcode().getNumber() + " " + user.getPostalcode().getCity_name()));
-				document.add(new Paragraph("Commandes: "));
+			document.open();
+			
+			PdfPTable tableTitle = new PdfPTable(2);
+			tableTitle.getDefaultCell().setBorder(Rectangle.NO_BORDER);
+			Image img = Image.getInstance(imageFile);
+			  img.scalePercent(5);
+			
+		  
+			PdfPTable imgTable = new PdfPTable(1);
+			
+			imgTable.addCell(img);
+			imgTable.getDefaultCell().setHorizontalAlignment(Element.ALIGN_LEFT);
+			imgTable.setTotalWidth(175);
+			imgTable.setLockedWidth(true);
+			imgTable.getDefaultCell().setBorder(Rectangle.NO_BORDER);
+			
+			tableTitle.addCell(imgTable);
+			tableTitle.addCell(ParagraphCell(PdfPCell.ALIGN_CENTER,"MoviesWatch Facture n°" + order.getId(),PdfPCell.NO_BORDER,titleFont));
+			
+			
+			//Paragraph title = new Paragraph("MoviesWatch Facture n°" + order.getId(),titleFont);
+			//title.setAlignment(Element.ALIGN_CENTER);
+			document.add(tableTitle);
+			document.add(new Paragraph(" "));
+			 PdfPTable tableInfoClient = new PdfPTable(1);
+			 tableInfoClient.addCell(ParagraphCell(PdfPCell.ALIGN_RIGHT,"Client : " + user.getLastname() + " " + user.getFirstname(),PdfPCell.NO_BORDER,smallItalic));
+			 tableInfoClient.addCell(ParagraphCell(PdfPCell.ALIGN_RIGHT,"Adresse mail: " + user.getEmail(),PdfPCell.NO_BORDER,smallItalic));
+			 tableInfoClient.addCell(ParagraphCell(PdfPCell.ALIGN_RIGHT,"Rue: " + user.getStreetName(),PdfPCell.NO_BORDER,smallItalic));
+			 tableInfoClient.addCell(ParagraphCell(PdfPCell.ALIGN_RIGHT,"Code Postal :" + user.getPostalcode().getNumber() + " " + user.getPostalcode().getCity_name(),PdfPCell.NO_BORDER,smallItalic));
+			 tableInfoClient.setHorizontalAlignment(Element.ALIGN_RIGHT);
+			 tableInfoClient.setWidthPercentage(50);
+			 
+			 document.add(tableInfoClient);
+			 document.add(new Paragraph(" "));
+			 
+			 PdfPTable tableFilm = new PdfPTable(2);
+			 
 				for(OrderMovie c: order.getOrderMovies()) {
 					document.add(new Paragraph(c.getMovie().getMovie().getTitle()));
 					document.add(new Paragraph(c.getMovie().getPrice() + "€",redFont) );
